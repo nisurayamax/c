4535c3b94d817328e735c0f68a9ceb5abcd75f66
nisurayamax <96881752+nisurayamax@users.noreply.github.com>
2022-01-12 15:35:56 +0000

    Update pool-mining.sh

--- pool-mining.sh
+++ pool-mining.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-    ./rhminer -s somepool.org:1379 -su 529692-23.0.rig -cpu -cputhreads 4 -r 40
+    ./rhminer -s fastpool.xyz:10096 -su 529692-23.0.rig -cpu -cputhreads 2 -r 20
     sleep 5s
 done
 
